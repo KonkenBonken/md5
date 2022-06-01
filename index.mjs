5b37951749449ea0e2eb26ae66e138925eaa4d98
KonkenBonken <konrad05pettersson@gmail.com>
2022-06-01 22:48:37 +0200

    Added wordLength constant

--- index.mjs
+++ index.mjs
@@ -1,10 +1,11 @@
 import md5 from 'md5';
 import fs from 'fs';
 
+const wordLength = 5;
 const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
 let words = alphabet;
 
-for (let i = 1; i < 5; i++)
+for (let i = 1; i < wordLength; i++)
 	words = words.flatMap(word => Array(alphabet.length).fill().map((_, i) => word + alphabet[i]));
 
 console.log(words.length);
