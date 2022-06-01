4e9577a553f71c66eb6eef5af37d31e09dae51f8
KonkenBonken <konrad05pettersson@gmail.com>
2022-06-01 23:41:53 +0200

    Added multithreading

--- index.mjs
+++ index.mjs
@@ -1,23 +1,43 @@
-console.time('All');
 import md5 from 'md5';
 import fs from 'fs';
+import { Worker, isMainThread, parentPort, workerData } from 'worker_threads';
 
-const wordLength = 5;
-const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
-let words = alphabet;
+if (isMainThread) {
+	const wordLength = 5;
+	const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+	let words = alphabet;
 
-for (let i = 1; i < wordLength; i++)
-	words = words.flatMap(word => Array(alphabet.length).fill().map((_, i) => word + alphabet[i]));
+	let threadCount = 10;
+	const threads = [];
+	const hashes = [];
 
-console.log('Words:', words.length);
+	console.time('Word generation');
+	for (let i = 1; i < wordLength; i++)
+		words = words.flatMap(word => Array(alphabet.length).fill().map((_, i) => word + alphabet[i]));
+	console.timeEnd('Word generation');
 
-console.time('Hashing');
-for (const i in words)
-	words[i] = md5(words[i]);
-console.timeEnd('Hashing');
+	console.log('Words:', words.length);
 
+	console.time('Hashing');
+	const wordsPerWorker = Math.floor(words.length / threadCount);
+	for (var i = 1; i < threadCount; i++)
+		threads.push(new Worker('./index.mjs', { workerData: { words: words.splice(0, wordsPerWorker), i } }));
+	threads.push(new Worker('./index.mjs', { workerData: { words, i } }));
 
-console.time('Save');
-fs.writeFileSync('hashes.txt', words.join('\n'))
-console.timeEnd('Save');
-console.timeEnd('All');
+	for (var worker of threads) {
+		worker.on('message', hashed => hashes.push(hashed));
+		worker.on('exit', () => {
+			if (--threadCount == 0) {
+				fs.writeFileSync('hashes.txt', hashes.flat().join('\n'))
+				console.timeEnd('Hashing');
+			}
+		})
+		worker.on('error', err => { throw err });
+	}
+
+} else {
+	console.log('Thread', workerData.i, 'initialized');
+	console.time(`Thread ${workerData.i} done`);
+	parentPort.postMessage(workerData.words.map(md5));
+	console.timeEnd(`Thread ${workerData.i} done`);
+}
